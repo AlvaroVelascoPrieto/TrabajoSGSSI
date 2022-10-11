df5c1b86a3675b1ae9ba46ad8a96e5cd32bb87db
Alvaro <alvaritovelasco2002@gmail.com>
2022-10-11 11:33:34 -0700

    Implementada la funcionalidad completa de edicion de tablas

--- app/ModificarTablas.php
+++ app/ModificarTablas.php
@@ -1,4 +1,28 @@
 <?php
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname,$username,$password,$db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+  
+  $query = mysqli_query($conn, "SELECT Modelo FROM carros") or die (mysqli_error($conn));
+  
+   if(isset($_POST['AplicarVictorias'])){
+	$query2 = mysqli_query($conn, "UPDATE carros SET Victorias='$_POST[NVictorias]' WHERE Modelo='$_POST[Dato1]'") or die (mysqli_error($conn));
+  }
+  else if(isset($_POST['AplicarPoles'])){
+	$query3 = mysqli_query($conn, "UPDATE carros SET Pole_positions='$_POST[Poles]' WHERE Modelo='$_POST[Dato1]'") or die (mysqli_error($conn));
+  }
+  else if(isset($_POST['AplicarPiloto'])){
+	$query4 = mysqli_query($conn, "UPDATE carros SET Primer_piloto='$_POST[piloto]' WHERE Modelo='$_POST[Dato1]'") or die (mysqli_error($conn));
+  }
+  else if(isset($_POST['AplicarAnno'])){
+	$query5 = mysqli_query($conn, "UPDATE carros SET Anno='$_POST[Anno]' WHERE Modelo='$_POST[Dato1]'") or die (mysqli_error($conn));
+  }
   echo "  
 <head>
     <title>Sing Up</title>
@@ -8,87 +32,64 @@
     <body class='bg'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/8/88/Lotus97T.jpg' style='width: 100%; height: auto;'>
         <div class='boxCentradoSobreImagen'>
+        	<form id='ModificarDatos' method='post'>
+        	<div class='formSeleccion'>
+                	<select name='Dato1'>";
+                
+        
+
+
+while ($option = mysqli_fetch_array($query)) {
+    echo "<option value='{$option[0]}'>{$option[0]}</option>";
+}
+
+	echo "  	</select>
+    		</div>
         	<div class='containerSeleccion'>
-        		<form id='ModificarNombre' method='post'>
 				<div class='formSeleccion'>
-					<h3>Nombre y Apellidos</h3>
+					<h3>Victorias</h3>
 				</div>
 				<div class='formSeleccion'>
-					<input class='usernameinput' id='NombreAp' name='NombreAp' placeholder='Ej: Pedro De La Rosa' align='center' required>
+					<input class='usernameinput' id='NVictorias' name='NVictorias' placeholder='Ej: 5' align='center'>
 				</div>
 				<div class='formSeleccion'>
-					<input type='submit' class='tag' name='AplicarNombre' value='Aplicar'/>
+					<input type='submit' class='tag' name='AplicarVictorias' value='Aplicar'/>
 				</div>
-			</form>
         	</div>
         	<div class='containerSeleccion'>
-        		<form id='ModificarDNI' method='post'>
 				<div class='formSeleccion'>
-					<h3>DNI</h3>
+					<h3>Pole Positions</h3>
 				</div>
 				<div class='formSeleccion'>
-					<input name='DNI' id='DNI' placeholder='Ej: 11111111X' required>
+					<input name='Poles' id='Poles' placeholder='Ej: 11'>
 				</div>
 				<div class='formSeleccion'>
-					<input type='submit' class='tag' name='AplicarDNI' value='Aplicar'/>
+					<input type='submit' class='tag' name='AplicarPoles' value='Aplicar'/>
 				</div>
-			</form>
         	</div>
         	<div class='containerSeleccion'>
-        		<form id='ModificarTelefono' method='post'>
 				<div class='formSeleccion'>
-					<h3>Telefono</h3>
+					<h3>Primer Piloto</h3>
 				</div>
 				<div class='formSeleccion'>
-					<input name='telefono' id='telefono' placeholder='Ej: 678901234' required>
+					<input name='piloto' id='piloto' placeholder='Ej: Fernando Alonso'>
 				</div>
 				<div class='formSeleccion'>
-					<input type='submit' class='tag' name='AplicarTelefono' value='Aplicar'/>
+					<input type='submit' class='tag' name='AplicarPiloto' value='Aplicar'/>
 				</div>
-			</form>
         	</div>
         	<div class='containerSeleccion'>
-        		<form id='ModificarFechaNac' method='post'>
 				<div class='formSeleccion'>
-					<h3>Fecha de nacimiento</h3>
+					<h3>Año</h3>
 				</div>
 				<div class='formSeleccion'>
-					<input name='fechaN' id='fechaN' type='date' required>
+					<input name='Anno' id='Anno' placeholder='2002'>
 				</div>
 				<div class='formSeleccion'>
-					<input type='submit' class='tag' name='AplicarFechaNac' value='Aplicar'/>
+					<input type='submit' class='tag' name='AplicarAnno' value='Aplicar'/>
 				</div>
-			</form>
         	</div>
-        	<div class='containerSeleccion'>
-        		<form id='ModificarEMail' method='post'>
-				<div class='formSeleccion'>
-					<h3>E-Mail</h3>
-				</div>
-				<div class='formSeleccion'>
-					<input name='email' id='email' type='text' placeholder='ejemplo@servidor.extension' required>
-				</div>
-				<div class='formSeleccion'>
-					<input type='submit' class='tag' name='AplicarEMail' value='Aplicar'/>
-				</div>
-			</form>
-        	</div>
-        	<div class='containerSeleccion'>
-        		<form id='ModificarContra' method='post'>
-				<div class='formSeleccion'>
-					<h3>Contrasena</h3>
-				</div>
-				<div class='formSeleccion'>
-					<input type='password' placeholder='Mínimo 8 caracteres' id='pw' name='pw' required>
-				</div>
-				<div class='formSeleccion'>
-					<input type='password' placeholder='Repita la contraseña' id='pw2' name='pw2' required>
-				</div>
-				<div class='formSeleccion'>
-					<input type='submit' class='tag' name='AplicarContra' value='Aplicar'/>
-				</div>
-			</form>
-        	</div>
+        	</form>
         </div>
     </body>";    
 ?>
