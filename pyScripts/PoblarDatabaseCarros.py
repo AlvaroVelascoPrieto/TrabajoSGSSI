0cc86e7d564a616f6a0b5e3fcfa569738129bfd3
AlbertoArostegui <aarosteguig@gmail.com>
2022-10-12 11:35:43 +0200

    Acabado (de cierta manera) el script que nos ayudará a poblar la base de datos

--- pyScripts/PoblarDatabaseCarros.py
+++ pyScripts/PoblarDatabaseCarros.py
@@ -5,9 +5,15 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
+import requests
+from bs4 import BeautifulSoup
+import re
 
 options = Options()
 #options.add_argument('--headless')
+
+#headers for BeautifulSoup
+headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36"}
 
 class Scripter:
 	def __init__(self):
@@ -15,10 +21,12 @@ class Scripter:
 		self.carLinks = {} #Dict to save all the different wikipedia pages of cars through history
 		
 	def getPageLinks(self, year): #Obtain all car links from a certain page
-		self.baseXPaths = '/html/body/div[3]/div[3]/div[5]/div[2]/div/div/div/div['
-		for i in range(1, 13):
-			row = self.baseXPaths + str(i) + "]/ul/li" #iterate Wikipedia letter markers
+
+		baseXPaths = '/html/body/div[3]/div[3]/div[5]/div[2]/div/div/div/div['
+		for i in range(1, 27):
+			row = baseXPaths + str(i) + "]/ul/li" #iterate Wikipedia letter markers
 			onlyOne = row + "/a"
+
 
 			try:
 				if year not in self.carLinks.keys():
@@ -29,7 +37,7 @@ class Scripter:
 			except NoSuchElementException:
 				pass
 
-			for j in range(2,8):
+			for j in range(2,10):
 				element = row + "[" + str(j) + "]/a"
 
 				try:
@@ -43,7 +51,7 @@ class Scripter:
 					pass
 
 	def fillCarLinksDict(self): #Must be executed to fill carLinks list
-		for i in range(1950, 1951): #2023 for till 2022
+		for i in range(1962, 2023): #2023 for till 2022
 			baseLinkA = "https://en.wikipedia.org/wiki/Category:"
 			baseLinkB = "_Formula_One_season_cars"
 
@@ -52,20 +60,117 @@ class Scripter:
 			self.getPageLinks(i)
 
 	def scrape(self):
-		for key in self.carLinks.keys():
-			for targetLink in self.carLinks[key]:
-				self.driver.get(targetLink)
-				try:
-					driversThTag = self.driver.find_element(By.XPATH, "//*[contains(text(), 'Notable drivers')]")
-					#Hasta aqui he llegado por ahora, basicamente tenemos un diccionario con los años y los links de todos los coches
-					#y he llegado al elemento <th> de Notable drivers en la caja de wikipedia, de ahí hay que buscar pasar a los
-					#pilotos en sí, que son un <a> pero anterior a ellos hay otro anchor para la bandera. Hay que buscar una manera
-					#igual con Xpath para saltarse un span y pasar a los pilotos
-					print(driversThTag)
+		for year in self.carLinks.keys():
+			for targetLink in self.carLinks[year]:
+				page = requests.get(targetLink)
+				soup = BeautifulSoup(page.content, "html.parser")
+				print("Scraping: " + targetLink)
+
+
+				try:  # Conseguir piloto
+					notableDriversTH = soup.find(text='Notable drivers').findNext('td').text
+					driversPreList = notableDriversTH.split(' ')
+					for i in range(0, len(driversPreList) - 1):
+						aux = driversPreList[i]
+						aux2 = re.sub(r'\d+', '', aux)
+						driversPreList[i] = aux2
+					piloto = ''
+					try:
+						for i in range(len(driversPreList)):
+							if driversPreList[i] != '' and len(piloto.split(' ')) < 2 and len(driversPreList[i]) > 4:
+								piloto = driversPreList[i] + ' ' + driversPreList[i + 1]
+					except IndexError:
+						pass
+				except AttributeError:
+					pass
+
+				try:  # Conseguir modelo
+					modelo = soup.find('span', {'class': 'mw-page-title-main'}).text
 				except NoSuchElementException:
 					pass
 
+				try:  # Conseguir poles y victorias
+					wins = soup.find(text='Wins').findNext('td').findNext('td')
+					poles = wins.findNext('td')
 
+					victorias = wins.text
+					poles = poles.text
+				except AttributeError:
+					pass
+
+				try:  # Conseguir foto
+					fotoLink = soup.find('td', {'class': 'infobox-image'}).findNext('img').get('src')
+					fotoLink = 'https:' + fotoLink
+
+				except AttributeError:
+					pass
+
+				try:
+					carro = {'year': year,
+							 'model': modelo,
+							 'piloto': piloto,
+							 'wins': int(victorias),
+							 'poles': int(poles),
+							 'foto': fotoLink}
+					self.toSQL(carro)
+				except ValueError:
+					pass
+
+
+
+
+	def scrapeOnlyOne(self, pageLink):
+		page = requests.get(pageLink)
+		soup = BeautifulSoup(page.content, "html.parser")
+
+		try: #Conseguir piloto
+			notableDriversTH = soup.find(text='Notable drivers').findNext('td').text
+			driversPreList = notableDriversTH.split(' ')
+			for i in range(0, len(driversPreList)-1):
+				aux = driversPreList[i]
+				aux2 = re.sub(r'\d+', '', aux)
+				driversPreList[i] = aux2
+			piloto = ''
+
+			for i in range(len(driversPreList)):
+				if driversPreList[i] != '' and len(piloto.split(' ')) < 2 and len(driversPreList[i]) > 4:
+					piloto = driversPreList[i] + ' ' + driversPreList[i + 1]
+		except NoSuchElementException:
+			pass
+
+		try: #Conseguir modelo
+			modelo = soup.find('span', {'class' : 'mw-page-title-main'}).text
+		except NoSuchElementException:
+			pass
+
+		try: #Conseguir poles y victorias
+			wins = soup.find(text='Wins').findNext('td').findNext('td')
+			poles = wins.findNext('td')
+
+			victorias = wins.text
+			poles = poles.text
+		except NoSuchElementException:
+			pass
+
+		try: #Conseguir foto
+			fotoLink = soup.find('td', {'class': 'infobox-image'}).findNext('img').get('src')
+			fotoLink = 'https:' + fotoLink
+
+		except NoSuchElementException:
+			pass
+
+		print(modelo)
+		print(piloto)
+		print(victorias)
+		print(poles)
+		print(fotoLink)
+
+	def toSQL(self, carro):
+		#('Ferrari F2004',15,12,'Michael Schumacher',2004, 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Ferrari_f2004.jpg/2560px-Ferrari_f2004.jpg')
+		f = open('queries.txt', 'a')
+		query = "('" + carro["model"] + '\',' + str(carro["wins"]) + ',' + str(carro["poles"]) + ',\'' + carro["piloto"] + '\',' + str(carro["year"]) + ',\'' + carro["foto"] + "'),"
+		f.write(query)
+		f.close
 
 
 
